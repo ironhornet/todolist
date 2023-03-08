0541000190d2ca9b8d2a78ed696ee64db0078b5d
Taras Myrka <Taras@MacBook-Pro-Admin.local>
2023-03-08 21:04:55 +0200

    add recompose

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { compose, withStateHandlers, withHandlers, lifecycle } from 'recompose';
 import { Button } from 'rsuite';
 
 import { deleteTodo, updateTodo } from '../../store/features/todo/todo.slice';
@@ -6,44 +6,92 @@ import { useAppDispatch } from '../../store/hooks/useAppDispatch';
 import { ITodoDto } from '../../types/todoList/todoList.interface';
 import { ModalPopup } from '../ModalPopup/ModalPopup';
 import { TodoItemView } from '../TodoItemView/TodoItemView';
-import { ITodoElementProps } from './todoItem.interface';
+import { IExhanceComponentProps } from './todoItem.interface';
 
-export const TodoItem: FC<ITodoElementProps> = (props) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [contentValue, setcontentValue] = useState(props.content || '');
-  const [titleValue, setTitleValue] = useState(props.title);
-  const [isOpen, setIsOpen] = useState(false);
+const exhanceComponent = compose<IExhanceComponentProps, {}>(
+  withStateHandlers(
+    {
+      contentValue: '',
+      titleValue: '',
+      isOpen: false,
+      isEditing: false,
+    },
+    {
+      setContentValue: () => (value) => ({ contentValue: value }),
+      setTitleValue: () => (value) => ({ titleValue: value }),
+      setIsOpen: () => (value) => ({ isOpen: value }),
+      setIsEditing: () => (value) => ({ isEditing: value }),
+    }
+  ),
+  lifecycle<IExhanceComponentProps, {}>({
+    componentDidMount() {
+      const { content, title, setContentValue, setTitleValue } = this.props;
+      if (content) setContentValue(content);
+      setTitleValue(title);
+    },
+  }),
+
+  withHandlers<IExhanceComponentProps, {}>({
+    handleContentChange:
+      ({ setContentValue }) =>
+      (value: string) => {
+        if (value.length <= 300) setContentValue(value);
+      },
+    handleTitleChange:
+      ({ setTitleValue }) =>
+      (value: string) => {
+        if (value.length <= 80) setTitleValue(value);
+      },
+    manageModal:
+      ({ setIsOpen }) =>
+      (value: boolean) => {
+        setIsOpen(value);
+      },
+    handleEdit:
+      ({ setIsEditing, isEditing }) =>
+      () => {
+        setIsEditing(!isEditing);
+      },
+  })
+);
+
+export const TodoItem = exhanceComponent((props) => {
+  const {
+    contentValue,
+    titleValue,
+    isOpen,
+    id,
+    title,
+    content,
+    done,
+    isEditing,
+    handleContentChange,
+    handleTitleChange,
+    setIsEditing,
+    manageModal,
+    handleEdit,
+    setIsOpen,
+  } = props;
 
   const dispatch = useAppDispatch();
 
   const handleDone = (todo: ITodoDto) => {
-    setIsEditing(false)
+    setIsEditing(false);
     dispatch(updateTodo(todo));
   };
-  const handleEdit = () => {
-    setIsEditing(!isEditing);
-  };
+
   const handleConfirmDelete = () => {
     dispatch(deleteTodo(props.id));
 
     setIsOpen(false);
   };
-  const manageModal = (value: boolean) => {
-    setIsOpen(value);
-  };
+
   const handleSave = (todo: ITodoDto) => {
     if (!titleValue) return;
 
     dispatch(updateTodo({ ...todo, content: contentValue, title: titleValue }));
     setIsEditing(false);
   };
-  const handleContentChange = (value: string) => {
-    if (contentValue.length <= 300) setcontentValue(value);
-  };
-  const handleTitleChange = (value: string) => {
-    if (titleValue.length <= 80) setTitleValue(value);
-  };
-  
 
   return (
     <li>
@@ -57,7 +105,7 @@ export const TodoItem: FC<ITodoElementProps> = (props) => {
         isEditing={isEditing}
         contentValue={contentValue}
         titleValue={titleValue}
-        todo={props}
+        todo={{ id, title, content, done }}
       />
 
       <ModalPopup
@@ -79,4 +127,4 @@ export const TodoItem: FC<ITodoElementProps> = (props) => {
       </ModalPopup>
     </li>
   );
-};
+});
